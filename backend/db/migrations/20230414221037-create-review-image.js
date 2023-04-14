667433ce929dda1f7c16bccd926dbbe7022044fd
Albert <Alberthsk115@gmail.com>
2023-04-14 15:15:05 -0700

    added reviewimage migration and model

--- backend/db/migrations/20230414221037-create-review-image.js
+++ backend/db/migrations/20230414221037-create-review-image.js
@@ -7,36 +7,31 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('SpotImages', {
+    return queryInterface.createTable('ReviewImages', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      spotId: {
+      reviewId: {
         type: Sequelize.INTEGER
       },
       url: {
         type: Sequelize.STRING
       },
-      preview: {
-        type: Sequelize.BOOLEAN
-      },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        type: Sequelize.DATE
       }
     });
   },
   down: async (queryInterface, Sequelize) => {
-    options.tableName = "SpotImages";
+    options.tableName = "ReviewImages";
     return queryInterface.dropTable(options);
   }
 };
